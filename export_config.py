27ba6f42c30f215d580ac56f01fff5b4f176b922
Haujet Zhao <1292756898@qq.com>
2026-02-10 18:39:30 +0800

    重新开始

--- export_config.py
+++ export_config.py
@@ -4,7 +4,7 @@ model_home = Path('~/.cache/modelscope/hub/models/Qwen').expanduser()
 
 
 # [源模型路径] 官方下载好的 SafeTensors 模型文件夹
-MODEL_DIR =  model_home / 'Qwen3-ASR-1.7B'
+MODEL_DIR =  model_home / 'Qwen3-ASR-0.6B'
 
 # [导出目标路径] 转换后的 ONNX, GGUF 和权重汇总目录
 EXPORT_DIR = r'./model'
